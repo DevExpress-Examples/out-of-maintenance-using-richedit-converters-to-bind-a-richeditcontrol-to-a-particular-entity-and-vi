09c96f1a85edb864e00264b1a7bd1b3495229454
DevExpressExampleBot <fakeEmail>
2023-01-26 18:17:58 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,23 +1,24 @@
-﻿Imports System.IO
+Imports System.IO
 Imports System.Windows
 
 Namespace RichEditBindingConvertersWpf
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub reBindingSource_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			reBindingSource.LoadDocument("test.html")
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub tbBindingSource_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			tbBindingSource.Text = File.ReadAllText("test.html")
-		End Sub
-	End Class
+        Private Sub reBindingSource_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.reBindingSource.LoadDocument("test.html")
+        End Sub
+
+        Private Sub tbBindingSource_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.tbBindingSource.Text = File.ReadAllText("test.html")
+        End Sub
+    End Class
 End Namespace
